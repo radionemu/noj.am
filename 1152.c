54028382202e65550b9d6c1a682b1a51d0ec40cc
radionemu <85119559+radionemu@users.noreply.github.com>
2021-08-07 23:53:11 +0900

    Update 1152.c

--- 1152.c
+++ 1152.c
@@ -2,13 +2,17 @@
 
 int main(){
     char arr[1000001] = {0, };
-    scanf("%[^\n]", &arr);
+    gets(arr);
     int init = 0;
     int result = 0;
     //계산
     
     for(int i = 0; arr[i] < 1000001; i++){
         if((int)arr[i] == 32){
+            if((int)arr[i+1] == 0){
+                printf("0");
+                return 0;
+            }
             continue;
         }else if((int)arr[i] != 32 && (int)arr[i] !=0){
             init = i;
@@ -22,6 +26,9 @@ int main(){
     for(int j = init; arr[j] < 1000001; j++){
         if((int)arr[j] == 32){
             result ++;
+            if((int)arr[j+1] == 0){
+                break;
+            }
             continue;
         }
         if((int)arr[j] == 0){
