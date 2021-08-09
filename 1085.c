8714b536c3cbdfa8af5ec61f201ee9fae8332f5f
radionemu <85119559+radionemu@users.noreply.github.com>
2021-08-09 15:08:38 +0900

    Update 1085.c

--- 1085.c
+++ 1085.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 int main(){
-    int x, y, w, h;
+    int x, y, w, h, a, b;
     scanf("%d %d %d %d", &x, &y, &w, &h);
-    printf("%d", ((w-x) > (h-y)) ? w-x : h-y);    
+    a = x > w-x ? w-x : x, b = y > h-y ? h-y : y;
+    printf("%d", a > b ? b : a);    
     return 0;
 }
