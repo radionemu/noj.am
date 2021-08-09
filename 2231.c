7bb21506c5a0c8e004058c5a268470915e8657d2
radionemu <85119559+radionemu@users.noreply.github.com>
2021-08-09 15:27:51 +0900

    Create 2231.c

---
+++ 2231.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int d(int n);
+
+int main(){
+    int input;
+    ascanf ("%d", &input);
+    for(int i = 1; i < input; i++){
+        if (d(i) == input){
+            printf("%d", i);
+            return 0;
+        }
+    }
+    printf("0");
+
+    return 0;
+}
+
+int d(int n){
+    int result = n + (n/1000000) + (n%1000000)/100000 + (n%100000)/10000 + (n%10000)/1000 + (n%1000)/100 + (n%100)/10 + (n%10);
+    return result;
+}
